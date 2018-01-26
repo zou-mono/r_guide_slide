be5d2e5b1024ff58f58ee13d2517797069bfd05a
zou-mono <zou.mono@gmail.com>
2018-01-26 17:26:19 +0800

    完成统计元素-颜色部分.

--- figure.R
+++ figure.R
@@ -56,6 +56,7 @@ showCols1()
 dev.off()
 
 png("colors-bar.png")
+opar <- par()
 par(mar = c(0, 6, 0, 0) + 0.1, height=30, yaxs = "i")
 barplot(rep(1,20), col = c("tomato","lightcyan4","navajowhite1",
 "gray5","seagreen3","ghostwhite","grey60","cornsilk4","linen","darkblue",
@@ -65,12 +66,75 @@ barplot(rep(1,20), col = c("tomato","lightcyan4","navajowhite1",
 "gray26","steelblue1","grey","plum4","violet","gray83","red",
 "thistle1","palegreen4","oldlace"), horiz = TRUE, las = 1,
 xaxt = "n")
+par(opar)
 dev.off()
 
-png("color-example.png",width=1600,height=600,res=300,pointsize=3)
-library(MSG)
-data(BinormCircle)
-par(mfrow = c(1, 2), pch = 20, ann = FALSE, mar = c(2, 2 + 2, 0.5, 0.2))
-plot(BinormCircle, col = rgb(1, 0, 0))
-plot(BinormCircle, col = rgb(1, 0, 0, alpha = 0.01))
+png("colors-bar.png")
+opar <- par()
+par(mar = c(0, 6, 0, 0) + 0.1, height=30, yaxs = "i")
+barplot(rep(1,20), col = c("tomato","lightcyan4","navajowhite1",
+"gray5","seagreen3","ghostwhite","grey60","cornsilk4","linen","darkblue",
+"gray26","steelblue1","grey","plum4","violet","gray83","red",
+"thistle1","palegreen4","oldlace"),names.arg = c("tomato","lightcyan4","navajowhite1",
+"gray5","seagreen3","ghostwhite","grey60","cornsilk4","linen","darkblue",
+"gray26","steelblue1","grey","plum4","violet","gray83","red",
+"thistle1","palegreen4","oldlace"), horiz = TRUE, las = 1,
+xaxt = "n")
+par(opar)
 dev.off()
+
+png("rgb-bar.png",width=1600,height=600,res=300,pointsize=3)
+par(mar = c(0.2, 2, 1, 0) + 0.1, yaxs = "i")
+x = rgb(1, seq(0, 1, length = 20), 0)
+barplot(rep(1, 20), col = x)
+dev.off()
+
+require(datasets)
+#require(grDevices); require(graphics)
+png("terrain_colors.png")
+par(mar = c(2,2,2,0) + 0.1)
+x <- 10*(1:nrow(volcano)); x.at <- seq(100, 800, by=100)
+y <- 10*(1:ncol(volcano)); y.at <- seq(100, 600, by=100)
+# Using Terrain Colors 
+image(x, y, volcano, col=terrain.colors(100),axes=FALSE,xlab="",ylab="")
+contour(x, y, volcano, levels=seq(90, 200, by=5), add=TRUE, col="brown")
+axis(1, at=x.at)
+axis(2, at=y.at)
+box()
+title(main="col=terrain.colors(100)", font.main=4)
+dev.off()
+
+png("heat_colors.png")
+par(mar = c(2,2,2,0) + 0.1)
+image(x, y, volcano, col=heat.colors(100), axes=FALSE)
+contour(x, y, volcano, levels=seq(90, 200, by=5), add=TRUE, col="brown")
+axis(1, at=x.at)
+axis(2, at=y.at)
+box()
+title(main="col=heat.colors(100)", font.main=4)
+dev.off()
+
+png("gray_colors.png")
+par(mar = c(2,2,2,0) + 0.1)
+image(x, y, volcano, col=gray(100:200/200), axes=FALSE)
+contour(x, y, volcano, levels=seq(90, 200, by=5), add=TRUE, col="black")
+axis(1, at=x.at)
+axis(2, at=y.at)
+box()
+title(main="col=gray(100:200/200)", font.main=4)
+dev.off()
+
+png("rcolorbrewer.png")
+layout(matrix(1:3, 3), heights = c(2, 1, 1))
+par(mar = c(0, 4, 0, 0))
+# 连续型:18种
+display.brewer.all(type = "seq")
+# 极端化:9种
+display.brewer.all(type = "div")
+# 离散型:8种
+display.brewer.all(type = "qual")
+dev.off()
+
+
+
+
