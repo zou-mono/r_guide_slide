417bcfa0cb7b8d1879c386fe7154db088cef7559
zou-mono <zou.mono@gmail.com>
2018-01-29 00:29:10 +0800

    完成统计图形元素-文本和图例部分.

--- figure.R
+++ figure.R
@@ -256,7 +256,37 @@ par(mar = c(2.5,2.5,0,0) + 0.1)
 with(iris,
      {
      plot(Sepal.Length, Sepal.Width, col = as.integer(Species),
-          panel.first = grid(3, lty = 1, lwd = 2))
+          panel.first = grid(8, lty = 1, lwd = 2))
      }
      )
+dev.off()
+
+png("text-example.png",width=1000,height=1000,res=300,pointsize=6)
+par(mar = c(4, 4, 4, 3))
+plot(0:10, type = "n", xlab = "", ylab = "", xlim = c(0,12))
+grid(col = "gray")
+title(main = "Demonstration of text in R Graphics",
+xlab = "X-axis title", ylab = "Y-axis title")
+mtext("Here is \"side = 4\"", side = 4, line = 1)
+x = c(6, 4, 6, 8)
+y = c(8, 5, 2, 5)
+s = c(0, 90, 180, 270)
+for (i in 1:4) text(x[i], y[i], sprintf("srt = %d",s[i]), srt = s[i])
+segments(c(6, 0, 6, 12), c(10, 5, 0, 5), c(0, 6,12, 6), c(5, 0, 5, 10), lty = c(2, 1, 1, 2))
+
+dev.off()
+
+png("legend-example.png",width=1000,height=1000,res=300,pointsize=6)
+par(mar = c(4, 4, 4, 3))
+plot(0:10, type = "n", xlab = "", ylab = "", xlim = c(0,12))
+grid(col = "gray")
+title(main = "Demonstration of text in R Graphics",
+xlab = "X-axis title", ylab = "Y-axis title")
+mtext("Here is \"side = 4\"", side = 4, line = 1)
+x = c(6, 4, 6, 8)
+y = c(8, 5, 2, 5)
+s = c(0, 90, 180, 270)
+for (i in 1:4) text(x[i], y[i], sprintf("srt = %d",s[i]), srt = s[i])
+segments(c(6, 0, 6, 12), c(10, 5, 0, 5), c(0, 6,12, 6), c(5, 0, 5, 10), lty = c(2, 1, 1, 2))
+legend(-0.2, 9.8, c("Upper", "Lower"), lty = 2:1, cex = 0.8, bty = "n",text.col="red",col="red")
 dev.off()
