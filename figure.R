1583b1266e33db76455d22302f42d864fb5ca92e
zou-mono <zou.mono@gmail.com>
2018-02-08 16:35:06 +0800

    增加线的艺术

--- figure.R
+++ figure.R
@@ -681,3 +681,19 @@ dev.off()
 CairoPNG("show_settings.png",width=1200*3,height=800*3,res=72*5)
 show.settings()
 dev.off()
+
+CairoPNG("line-art1.png",width=1000*3,height=1000*3,res=72*3)
+x1 = c(seq(0, pi, length = 50), seq(pi, 2*pi, length = 50))
+y1 = cos(x1) / sin(x1)
+x2 = seq(1.02 * 2 * pi + pi/2, 4*pi + pi/2, length = 50)
+y2 = tan(x2)
+op = par(bg="black", mar=rep(.5,4))
+plot(c(x1, x2), c(y1, y2), type = "n", ylim = c(-11, 11))
+for (i in seq(-10, 10, length = 100))
+{
+  lines(x1, y1 + i, col = hsv(runif(1,.65,.7), 1, 1, runif(1,.7)),
+        lwd = 4 * runif(1, 0.3))
+  lines(x2, y2 + i, col = hsv(runif(1,.65,.7), 1, 1, runif(1,.7)),
+        lwd = 4 * runif(1, 0.3))
+}
+dev.off()
